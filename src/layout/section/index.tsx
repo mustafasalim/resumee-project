2635e10c058ee355de4a72acf9c98f1d745673d9
Mustafa Salim <salimmustafa763@gmail.com>
2023-12-30 17:00:30 +0300

    #2 QR code section has been updated. QR code with icon has been used. vercel has been added

--- src/layout/section/index.tsx
+++ src/layout/section/index.tsx
@@ -1,8 +1,7 @@
-import { QRCode, Space } from 'antd';
+import { QRCode, Space } from "antd"
 function Section() {
   return (
     <section>
-
       <div className="flex justify-evenly w-full mt-40">
         <div className="text-black text-[64px] font-extrabold">
           We made CV designs
@@ -20,19 +19,29 @@ function Section() {
         <div className="flex  justify-center mt-10 mb-10 w-[1702px] h-px bg-zinc-300" />
       </div>
 
-      <div className='mt-20 mb-40'>
-        <div className='w-full flex items-center justify-center'>
-          <Space>
-
-            <QRCode type="svg" value="https://github.com/mustafasalim/resumee-project" />
+      <div className="mt-20 mb-40">
+        <div className="w-full flex items-center justify-center">
+          <Space className="flex gap-10">
+            <QRCode
+             className=""
+              errorLevel="H"
+              value="https://github.com/mustafasalim/resumee-project"
+              icon="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            />
+            <QRCode
+              errorLevel="H"
+              value="https://resumee-project.vercel.app/"
+              icon="https://www.svgrepo.com/show/327408/logo-vercel.svg"
+            />
           </Space>
         </div>
 
-        <div className='text-black text-[40px] font-extrabold text-center  w-full flex items-center justify-center'>
-        <div className='mt-10'>
-        You can access the <span className='text-[#2C7FFB]'>github</span> page<br/>
-          of the project by scanning the QR CODE.
-        </div>
+        <div className="text-black text-[40px] font-extrabold text-center  w-full flex items-center justify-center">
+          <div className="mt-10">
+            You can access the <span className="text-[#2C7FFB]">github</span>{" "}
+            and <br /> <span className="text-[#2C7FFB]">vercel</span> project by
+            scanning the QR code.
+          </div>
         </div>
       </div>
     </section>
